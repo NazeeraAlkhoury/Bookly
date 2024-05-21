f0a97b6da8be491d69cd86883984709a657a5aaa
Nazeera Alkhouri <nazeeraalkhoury21@gmail.com>
2024-05-21 17:45:24 +0300

    Add AppStyles to our app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookly/core/utils/app_colors.dart';
 import 'package:bookly/core/utils/app_routes.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -17,6 +18,7 @@ class BooklyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: AppColors.kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       ),
       home: const SplashView(),
       onGenerateRoute: RouteGenerator.getRoutes,
